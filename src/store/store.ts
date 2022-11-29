31458088c78e26cb8e8be2b5faec42452ae08db4
Alexander Castro <alexander.castro01@uptc.edu.co>
2022-11-29 00:23:41 -0500

    Se mejora el reporte comparativo

--- src/store/store.ts
+++ src/store/store.ts
@@ -9,8 +9,6 @@ export const store = reactive({
       const test = this.testList.find(function (element: any[]) {
         return element[0] === data[0] && element[1] === data[1];
       });
-      console.log(data[0]);
-      console.log(test);
       newList.push([data[0], data[1], data[2], data[3], data[4], test[2], test[3], test[4]]);
     }
     return newList;
